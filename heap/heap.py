7425d2fd0108ec3b0eeeac8794cd50cf8f1f136f
Maksim Zholudev <maximzms@gmail.com>
2022-08-21 15:30:56 +0300

    Add Heap.GetMax

--- heap/heap.py
+++ heap/heap.py
@@ -13,31 +13,61 @@ class Heap:
             self.Add(key)
 
     def GetMax(self):
-        # вернуть значение корня и перестроить кучу
-        return -1 # если куча пуста
+        if self.__size == 0:
+            return -1
+        if self.__size == 1:
+            return self.__get_last()
+        
+        root = self.HeapArray[0]
+        self.HeapArray[0] = self.__get_last()
+        self.__sift_down(0)
+        return root
 
     def Add(self, key):
         if self.__size == self.__capacity:
             return False
-        index = self.__size
-        self.HeapArray[index] = key
-        self.__size += 1
-        
-        while index > 0:
-            parent_index = _parent_index(index)
-            
-            if self.HeapArray[parent_index] > self.HeapArray[index]:
-                break
-
-            tmp = self.HeapArray[index]
-            self.HeapArray[index] = self.HeapArray[parent_index]
-            self.HeapArray[parent_index] = tmp
-            del tmp
-
-            index = parent_index
-            del parent_index
-        
+        self.__add_to_end(key)
+        self.__sift_up(self.__size - 1)
         return True
+
+    def __get_last(self):
+        self.__size -= 1
+        key = self.HeapArray[self.__size]
+        self.HeapArray[self.__size] = None
+        return key
+
+    def __sift_down(self, index):
+        left = _left_child_index(index)
+        if left >= self.__size:
+            return
+        right = _right_child_index(index)
+        go_left = right >= self.__size or self.HeapArray[right] < self.HeapArray[left]
+        target = left if go_left else right
+
+        if self.__fix_order(index, target):
+            self.__sift_down(target)
+
+    def __add_to_end(self, key):
+        self.HeapArray[self.__size] = key
+        self.__size += 1
+
+    def __sift_up(self, index):
+        if index == 0:
+            return
+        parent = _parent_index(index)
+        if self.__fix_order(parent, index):
+            self.__sift_up(parent)
+
+    def __fix_order(self, parent, child):
+        if self.HeapArray[parent] > self.HeapArray[child]:
+            return False
+        self.__swap(parent, child)
+        return True
+
+    def __swap(self, i1, i2):
+        tmp = self.HeapArray[i1]
+        self.HeapArray[i1] = self.HeapArray[i2]
+        self.HeapArray[i2] = tmp
 
 
 def _left_child_index(i):
--- heap/test_heap.py
+++ heap/test_heap.py
@@ -229,5 +229,120 @@ class Test(unittest.TestCase):
         self.assertFalse(h.Add(2))
 
 
+    def test_get(self):
+        h = Heap()
+        self.assertEqual(h.GetMax(), -1)
+        self.assertEqual(h.HeapArray, [])
+        
+        h.MakeHeap([], 0)
+        self.assertEqual(h.GetMax(), -1)
+        self.assertEqual(h.HeapArray, [None])
+        
+        h.Add(20)
+        self.assertEqual(h.GetMax(), 20)
+        self.assertEqual(h.HeapArray, [None])
+
+        h.MakeHeap([20], 3)
+        self.assertEqual(h.GetMax(), 20)
+        self.assertEqual(h.HeapArray, [None] * 15)
+
+        h.MakeHeap([20, 10, 30, 5, 15, 25, 7, 17, 22, 35, 33, 31, 37, 1, 40], 3)
+        self.assertEqual(h.HeapArray, [
+            40,
+            33, 37,
+            17, 30, 31, 35,
+            5, 15, 10, 22, 20, 25, 1, 7
+            ])
+        self.assertEqual(h.GetMax(), 40)
+        self.assertEqual(h.HeapArray, [
+            37,
+            33, 35,
+            17, 30, 31, 7,
+            5, 15, 10, 22, 20, 25, 1, None
+            ])
+        self.assertEqual(h.GetMax(), 37)
+        self.assertEqual(h.HeapArray, [
+            35,
+            33, 31,
+            17, 30, 25, 7,
+            5, 15, 10, 22, 20, 1, None, None
+            ])
+        self.assertEqual(h.GetMax(), 35)
+        self.assertEqual(h.HeapArray, [
+            33,
+            30, 31,
+            17, 22, 25, 7,
+            5, 15, 10, 1, 20, None, None, None
+            ])
+        self.assertEqual(h.GetMax(), 33)
+        self.assertEqual(h.HeapArray, [
+            31,
+            30, 25,
+            17, 22, 20, 7,
+            5, 15, 10, 1, None, None, None, None
+            ])
+        self.assertEqual(h.GetMax(), 31)
+        self.assertEqual(h.HeapArray, [
+            30,
+            22, 25,
+            17, 10, 20, 7,
+            5, 15, 1, None, None, None, None, None
+            ])
+        self.assertEqual(h.GetMax(), 30)
+        self.assertEqual(h.HeapArray, [
+            25,
+            22, 20,
+            17, 10, 1, 7,
+            5, 15, None, None, None, None, None, None
+            ])
+        self.assertEqual(h.GetMax(), 25)
+        self.assertEqual(h.HeapArray, [
+            22,
+            17, 20,
+            15, 10, 1, 7,
+            5, None, None, None, None, None, None, None
+            ])
+        self.assertEqual(h.GetMax(), 22)
+        self.assertEqual(h.HeapArray, [
+            20,
+            17, 7,
+            15, 10, 1, 5,
+            ] + [None] * 8)
+        self.assertEqual(h.GetMax(), 20)
+        self.assertEqual(h.HeapArray, [
+            17,
+            15, 7,
+            5, 10, 1, None,
+            ] + [None] * 8)
+        self.assertEqual(h.GetMax(), 17)
+        self.assertEqual(h.HeapArray, [
+            15,
+            10, 7,
+            5, 1, None, None,
+            ] + [None] * 8)
+        self.assertEqual(h.GetMax(), 15)
+        self.assertEqual(h.HeapArray, [
+            10,
+            5, 7,
+            1, None, None, None,
+            ] + [None] * 8)
+        self.assertEqual(h.GetMax(), 10)
+        self.assertEqual(h.HeapArray, [
+            7,
+            5, 1,
+            ] + [None] * 12)
+        self.assertEqual(h.GetMax(), 7)
+        self.assertEqual(h.HeapArray, [
+            5,
+            1, None,
+            ] + [None] * 12)
+        self.assertEqual(h.GetMax(), 5)
+        self.assertEqual(h.HeapArray, [1] + [None] * 14)
+        self.assertEqual(h.GetMax(), 1)
+        self.assertEqual(h.HeapArray, [None] * 15)
+        self.assertEqual(h.GetMax(), -1)
+        self.assertEqual(h.HeapArray, [None] * 15)
+        
+
 if __name__ == "__main__":
     unittest.main()
